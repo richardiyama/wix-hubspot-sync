e9af72ace26133365908112eab08f4d1b761567a
richardiyama <richardiyama@yahoo.com>
2026-05-23 18:15:46 +0100

    Initial commit from Create Wix App

---
+++ src/extensions/dashboard/pages/my-page/my-page.extension.ts
@@ -0,0 +1,8 @@
+import { extensions } from '@wix/astro/builders';
+
+export default extensions.dashboardPage({
+  id: 'a210bccc-921e-4f6b-b13a-4475b792d508',
+  title: 'My Page',
+  routePath: '',
+  component: './extensions/dashboard/pages/my-page/my-page.tsx',
+});
